355e575133a23e4eb4b1edb9c1be6c10fab3c4b3
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-10-27 10:34:27 +0700

    mongo

---
+++ MongoDB_SP/App_Start/MongoDBContext.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports System.Web.Routing
+
+Public Class MongoDBContext
+   MongoClient
+
+     
+
+End Class
---
+++ MongoDB_SP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="MongoDB_SP.MvcApplication" Language="VB" %>
